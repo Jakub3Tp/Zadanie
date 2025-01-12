0ec7286cf04765ac14d84c6d61e8c9e125f8065f
kubix <jakub.gola2@teb.edu.pl>
2025-01-12 11:46:07 +0100

    12.01.2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  //const [count, setCount] = useState(0)
+    function handleSubmit(event){
+        event.preventDefault();
+        const formData = new FormData(event.target);
+
+        // Pobierz tytuł filmu
+        const title = formData.get("title");
+        console.log(`Tytuł filmu: ${title}`);
+
+        // Pobierz wybrany gatunek
+        const genre = formData.get("movie");
+        console.log(`Wybrany gatunek: ${genre}`);
+    }
 
   return (
       <>
-          <div className="container">
-              <div className="mb-3">
-                  <label className="form-label">Tytuł filmu</label>
-                  <input type="text" className="form-control"/>
+          <form onSubmit={handleSubmit} method='post'>
+              <div className="container">
+                  <div className="mb-3">
+                      <label className="form-label">Tytuł filmu</label>
+                      <input type="text" className="form-control" name="title"/>
+                  </div>
+                  <div className="mb-3">
+                      <label className="form-label">
+                          Gatunek filmu:
+                      </label>
+                      <select className="form-select" name='movie'>
+                          <option id={"1"}>Komedia</option>
+                          <option id={"2"}>Obyczajowy</option>
+                          <option id={"3"}>Sensacyjny</option>
+                          <option id={"4"}>Horror</option>
+                      </select>
+                  </div>
+                  <div className="mb-3">
+                      <button type="submit" className="btn btn-primary">Dodaj</button>
+                  </div>
               </div>
-              <div className="mb-3">
-                  <label className="form-label">
-                      Gatunek filmu:
-                  </label>
-                  <select className="form-select">
-                      <option id={"1"}>Komedia</option>
-                      <option id={"2"}>Obyczajowy</option>
-                      <option id={"3"}>Sensacyjny</option>
-                      <option id={"4"}>Horror</option>
-                  </select>
-              </div>
-              <div className="mb-3">
-                  <button type="button" className="btn btn-primary">Dodaj</button>
-              </div>
-          </div>
-      </>
-  )
-}
+              </form>
+          </>
+          )
+          }
 
-export default App
+          export default App
